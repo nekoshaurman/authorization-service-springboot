8060c6485e7c145cf7051ff454844ccd53ccb0ad
nekoshaurman <mrrft2@gmail.com>
2025-07-22 13:22:22 +0300

    Initial commit

---
+++ src/main/java/neko/authorizatio/service/AuthorizationServiceSpringbootApplication.java
@@ -0,0 +1,13 @@
+package neko.authorizatio.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServiceSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServiceSpringbootApplication.class, args);
+    }
+
+}
