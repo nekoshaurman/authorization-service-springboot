73f7d4758a8f394f92badc0da3fe739798b53d60
nekoshaurman <mrrft2@gmail.com>
2025-07-22 13:39:16 +0300

    Base model

---
+++ src/main/java/neko/authorization/service/model/Role.java
@@ -0,0 +1,7 @@
+package neko.authorization.service.model;
+
+public enum Role {
+    ADMIN,
+    PREMIUM_USER,
+    GUEST
+}
---
+++ src/main/java/neko/authorization/service/model/User.java
@@ -0,0 +1,37 @@
+package neko.authorization.service.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 4, max = 20)
+    @Column(unique = true)
+    private String login;
+
+    @NotBlank
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String passwordHash;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+}
